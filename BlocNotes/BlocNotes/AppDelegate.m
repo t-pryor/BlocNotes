89b8b0f9a3c6edc5dec127209113bc7c18a76dd1
Tim Pryor <Tim@Chantals-MacBook-Pro.local>
2015-07-15 11:06:12 -0400

    Simplified split view controller code in the AppDelegate.

--- BlocNotes/BlocNotes/AppDelegate.m
+++ BlocNotes/BlocNotes/AppDelegate.m
@@ -64,18 +64,13 @@
 
 #pragma mark - Split View Delegate
 
-- (BOOL)splitViewController:(UISplitViewController *)splitViewController collapseSecondaryViewController:(UIViewController *)secondaryViewController ontoPrimaryViewController:(UIViewController *)primaryViewController {
-    if ([secondaryViewController isKindOfClass:[UINavigationController class]] && [[(UINavigationController *)secondaryViewController topViewController] isKindOfClass:[DetailViewController class]] && ([(DetailViewController *)[(UINavigationController *)secondaryViewController topViewController] detailItem] == nil)) {
-        // Return YES to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-        return YES;
-    } else {
-        return NO;
-    }
+-(BOOL)splitViewController:(UISplitViewController *)splitViewController collapseSecondaryViewController:(UIViewController *)secondaryViewController ontoPrimaryViewController:(UIViewController *)primaryViewController
+{
+    // for iPhone, second VC thrown away
+    return YES;
 }
 
-
-
-
+// for iPhone 6 plus in portrait, default behavior is to expand to show detail view controller. We leave it as is.
 
 
 
